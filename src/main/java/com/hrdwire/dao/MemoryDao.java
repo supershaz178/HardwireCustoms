15bbf4907854ec7d643331144256c701dca6607b
supershaz178 <supershaz178@users.noreply.github.com>
2018-06-16 18:22:39 -0500

    Added MemoryDAO interface

---
+++ src/main/java/com/hrdwire/dao/MemoryDao.java
@@ -0,0 +1,32 @@
+package com.hrdwire.dao;
+
+import java.util.List;
+
+import com.hrdwire.persistence.Memory;
+
+public interface MemoryDao extends BaseDao<Memory>
+{
+	public Memory selectByFullName(String fullName); 
+	
+	public List<Memory> searchByCasLatency(Integer casLatency);
+	
+	public List<Memory> searchByNumberOfModules(Integer modules); 
+	
+	public List<Memory> searchByNumberOfPins(Integer numOfPins); 
+	
+	public List<Memory> searchByMemorySpeed(String speed); 
+	
+	public List<Memory> searchByVoltage(Double voltage); 
+	
+	public List<Memory> searchByColor(String color); 
+	
+	public List<Memory> searchByCapacity(Integer Capacity); 
+	
+	public List<Memory> searchByTiming(String timing); 
+	
+	public List<Memory> searchByPartialName(String partialName); 
+	
+	public List<Memory> searchByCapacityAndNumOfModules(Integer capacity, Integer numOfModules); 
+	
+	public List<Memory> searchByHeatSpreadRating(String heatSpread); 
+}

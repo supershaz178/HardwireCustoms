97381b4ab2d6863bfcee2d5c2053d89424290c7d
supershaz178 <supershaz178@users.noreply.github.com>
2018-11-24 02:16:06 -0600

    Add CustomerService

---
+++ src/main/java/com/hrdwire/service/CustomerService.java
@@ -0,0 +1,27 @@
+package com.hrdwire.service;
+
+import java.sql.Date;
+import java.util.List;
+
+import com.hrdwire.persistence.Build;
+import com.hrdwire.persistence.Customer;
+
+public interface CustomerService extends BaseService<Customer>{
+	
+	public void assignCustNumber(Customer customer); 
+	
+	public Customer payForBuild(Customer customer); 
+	
+	public Build requestBuild(Customer requestCust);
+	
+	public void cancelBuild(Customer build);
+	
+	public List<Build> listAllOwnedBuilds(Customer customer); 
+	
+	public List<Customer> listAllCustomersFromDay(Date date); 
+	
+	public Customer editPersonalInfo(Customer customer); 
+	
+	public List<Customer> listAllCustomers(); 
+
+}

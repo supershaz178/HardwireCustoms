b2a9d1636e9602071f98b8977396d4b6198f6dea
supershaz178 <supershaz178@users.noreply.github.com>
2018-12-15 18:21:27 -0600

    Adding EmployeeService

---
+++ src/main/java/com/hrdwire/service/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.hrdwire.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.hrdwire.dao.BaseDao;
+import com.hrdwire.persistence.Build;
+import com.hrdwire.persistence.Customer;
+import com.hrdwire.persistence.Employee;
+
+public interface EmployeeService extends BaseDao<Employee> {
+	
+	public Employee createEmployeeAccount(Employee newEmp); 
+	
+	public Employee assignBuild(Build build); 
+	
+	public Employee changeAssignment(Build build); 
+	
+	public Employee assignCustomerService(Customer cust);
+	
+	public void updateProfile(Employee employee); 
+	
+	public void updateSecurePassword(Employee employee, String pwd); 
+	
+	public Employee updateEmployeeStatus(Employee employee, String newStatus);
+	
+	public List<Employee> searchEmployee(HashMap<String, String> parameters);
+}

9a727d9ef2a48d3c863d1fbae0fda2d75a0ed7d5
supershaz178 <supershaz178@users.noreply.github.com>
2019-03-19 00:24:26 -0500

    Add new CaseService, update pom.xml with new dependencies

---
+++ src/main/java/com/hrdwire/service/CaseService.java
@@ -0,0 +1,21 @@
+package com.hrdwire.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.hrdwire.dao.BaseDao;
+import com.hrdwire.persistence.Case;
+
+public interface CaseService extends BaseDao<Case>{
+	
+	public Case updateDimensions(Double height, Double width, Double length, Case updateDim); 
+		
+	public Case updateCaseStatistics(Case updateCase);
+	
+	public Case updateCasePrice(Case priceCase, Double newPrice); 
+
+	public List<Case> searchForCaseProduct(HashMap<String, String> parameters); 
+	
+	public List<Case> listAllCases(); 
+
+}

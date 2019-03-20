4e882793026e50a76bade2237bfb4c4a98460b61
supershaz178 <supershaz178@users.noreply.github.com>
2019-03-20 01:09:25 -0500

    Add CoolingDeviceService

---
+++ src/main/java/com/hrdwire/service/CoolingDeviceService.java
@@ -0,0 +1,17 @@
+package com.hrdwire.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.hrdwire.dao.BaseDao;
+import com.hrdwire.persistence.CoolingDevice;
+
+public interface CoolingDeviceService extends BaseDao<CoolingDevice>
+{
+	public CoolingDevice updateCoolingDevice(CoolingDevice updateDevice); 
+	
+	public List<CoolingDevice> determineCompatibility(CoolingDevice compatibleDevice); 
+	
+	public List<CoolingDevice> searchByParameters(HashMap<String,String> parameters); 
+
+}

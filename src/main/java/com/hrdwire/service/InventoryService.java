f18e6e5d13ca1cb6bef1a8f6b27f4f24a33d5bb8
supershaz178 <supershaz178@users.noreply.github.com>
2019-03-26 16:51:22 -0500

    Create InventoryService

---
+++ src/main/java/com/hrdwire/service/InventoryService.java
@@ -0,0 +1,33 @@
+package com.hrdwire.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.hrdwire.persistence.Inventory;
+import com.hrdwire.persistence.InventoryItem;
+import com.hrdwire.persistence.Parts;
+import com.hrdwire.persistence.history.InventoryHistory;
+
+public interface InventoryService extends BaseService<Inventory>
+{
+	public String createReferenceNumber(); 
+	
+	public Inventory createInventoryList(List<Parts> inventoryItemChanges); 
+	
+	public List<Inventory> searchByParmeters(HashMap<String,String> parameters); 
+	
+	public Inventory addInventoryItem(Inventory modifyEntry, InventoryItem addItem); 
+	
+	public Inventory modifyInventory(Inventory modifyEntry); 
+	
+	public Inventory modifyInventoryItem(Inventory modifyEntry, InventoryItem modifyItem);
+	
+	public InventoryHistory transferRecordToHistTable(Inventory inventoryEntry);
+	
+	public Inventory calculateNumberOfPartsTotal(Inventory calculateParts); 
+	
+	public Inventory listCurrentInventory(); 
+	
+	public List<Inventory> listAllInventory(); 
+
+}

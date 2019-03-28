83c239e3a9543e698d4137f6ada693e1e73ef69b
supershaz178 <supershaz178@users.noreply.github.com>
2019-03-28 17:16:20 -0500

    Added InventoryItemService

---
+++ src/main/java/com/hrdwire/service/InventoryItemService.java
@@ -0,0 +1,24 @@
+package com.hrdwire.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.hrdwire.persistence.InventoryItem;
+import com.hrdwire.persistence.Parts;
+import com.hrdwire.persistence.history.InventoryItemHistory;
+
+public interface InventoryItemService extends BaseService<InventoryItem>
+{
+	public InventoryItemHistory transferInventoryItem(InventoryItem transferItem); 
+	
+	public List<InventoryItem> searchByParameters(HashMap<String,String> parameters); 
+	
+	public InventoryItem searchByPartId(Integer partId); 
+	
+	public List<InventoryItem> createListForInventoryEntry(List<Parts> changedInventory);
+	
+	public InventoryItem createNewInventoryItem(Parts newPart); 
+	
+	public void removeInventoryItemByPart(Integer partId); 
+
+}

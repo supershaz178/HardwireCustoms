ed5897663aded4b031e2972efa7098b7d680cabb
supershaz178 <supershaz178@users.noreply.github.com>
2019-03-29 11:49:27 -0500

    Add MemoryService

---
+++ src/main/java/com/hrdwire/service/MemoryService.java
@@ -0,0 +1,27 @@
+package com.hrdwire.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.hrdwire.persistence.Build;
+import com.hrdwire.persistence.Memory;
+
+public interface MemoryService extends BaseService<Memory>
+{
+	public Memory updateMemoryModuleDetails(Integer memId);
+	
+	public Boolean isSetofModules(Memory memSet); 
+	
+	public Build addModuleToBuild(Memory mem);
+	
+	public Build installPartialSet(Memory mem, Integer numOfModules); 
+	
+	public Build removeModuleFromBuild(Memory mem); 
+	
+	public List<Memory> searchByParmeters(HashMap<String,String> parmeters); 
+	
+	public String determineType(Memory memType); 
+	
+	public List<Memory> listAllModules(); 
+	 
+}

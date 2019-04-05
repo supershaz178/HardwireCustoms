598a428e9d907b1b537891d16bd5205842e24666
supershaz178 <supershaz178@users.noreply.github.com>
2019-04-05 13:18:20 -0500

    Add PowerSupply Service

---
+++ src/main/java/com/hrdwire/service/PowerSupplyService.java
@@ -0,0 +1,19 @@
+package com.hrdwire.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.hrdwire.persistence.Build;
+import com.hrdwire.persistence.PowerSupply;
+
+public interface PowerSupplyService extends BaseService<PowerSupply>
+{
+	public List<PowerSupply> searchByParameters(HashMap<String,String> parameters); 
+	
+	public Boolean isAbleToSupplyPower(PowerSupply newPowerSupply, Build build); 
+	
+	public Boolean canLast10Years(PowerSupply powerSupply); 
+	
+	public List<PowerSupply> listAllPowerSupply(); 
+
+}

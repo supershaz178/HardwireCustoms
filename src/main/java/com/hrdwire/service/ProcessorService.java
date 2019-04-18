fe721ea2f46e6ae6891e10668c97944f54ca2eae
supershaz178 <supershaz178@users.noreply.github.com>
2019-04-18 17:54:24 -0500

    Add Processor Service

---
+++ src/main/java/com/hrdwire/service/ProcessorService.java
@@ -0,0 +1,20 @@
+package com.hrdwire.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.hrdwire.persistence.Motherboard;
+import com.hrdwire.persistence.Processor;
+
+public interface ProcessorService extends BaseService<Processor>
+{
+	public List<Processor> searchByParameters(Map<String,String> parmeters);
+	
+	public Boolean isCompatibleWithMoherBoard(Motherboard motherBoardCheck);
+	
+	public Boolean isGraphicsIntegreated(Processor proc); 
+	
+	public Boolean isOverclockable(Processor proc); 
+	
+	public List<Processor> listAllProcessors(); 
+}

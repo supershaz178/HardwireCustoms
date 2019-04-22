068b028ec50a10fd35764b77a02190808a692e09
supershaz178 <supershaz178@users.noreply.github.com>
2019-04-22 16:56:34 -0500

    Added SolidStateDrive Service

---
+++ src/main/java/com/hrdwire/service/SolidStateDriveService.java
@@ -0,0 +1,21 @@
+package com.hrdwire.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.hrdwire.persistence.Build;
+import com.hrdwire.persistence.SolidStateDrive;
+
+public interface SolidStateDriveService extends BaseService<SolidStateDrive>
+{
+	public List<SolidStateDrive> searchByParameters(Map<String,String> parameters); 
+	
+	public Build removeSolidStateDrive(SolidStateDrive drive);
+	
+	public Build determineCompatibility(SolidStateDrive drive); 
+	
+	public Build addSoidStateDriveToBuild(SolidStateDrive addDrive);
+	
+	public List<SolidStateDrive> listAllSolidStateDrives(); 
+	
+}

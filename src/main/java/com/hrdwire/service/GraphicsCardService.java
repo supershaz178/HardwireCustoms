a5e67bc068bcd45ea1673258fd9151157d724506
supershaz178 <supershaz178@users.noreply.github.com>
2019-04-25 16:02:51 -0500

    Add GraphicsCardSerivice

---
+++ src/main/java/com/hrdwire/service/GraphicsCardService.java
@@ -0,0 +1,20 @@
+package com.hrdwire.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.hrdwire.persistence.Build;
+import com.hrdwire.persistence.GraphicsCard;
+
+public interface GraphicsCardService extends BaseService<GraphicsCard>
+{
+	public List<GraphicsCard> searchByParameters(Map<String,String> parameters); 
+	
+	public Build addGraphicsCardToBuild(Build build, GraphicsCard gCard); 
+	
+	public Build removeGraphicsCardFromBuild(Build build, GraphicsCard rmGCard); 
+	
+	public Boolean isSLIConfigured(Build build); 
+	
+	public List<GraphicsCard> listAllGraphicsCards(); 
+}

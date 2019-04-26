3f11ebfd26afb663fea7f781a17d733c4185b568
supershaz178 <supershaz178@users.noreply.github.com>
2019-04-26 14:45:05 -0500

    Format all Constant classes

--- src/main/java/com/hrdwire/constants/MemoryTypes.java
+++ src/main/java/com/hrdwire/constants/MemoryTypes.java
@@ -1,22 +1,22 @@
 package com.hrdwire.constants;
 
 public class MemoryTypes {
-	
-	//Desktop Memory Types
+
+	// Desktop Memory Types
 	public static final String DDR4 = "DDR4";
 	public static final String DDR3 = "DDR3";
 	public static final String DDR2 = "DDR2";
 	public static final String DDR = "DDR";
 	public static final String SDRAM_168_PIN = "168 PIN SDRAM";
 	public static final String SDRAM_184_PIN = "184 PIN SDRAM";
-	
-	//Graphics Memory Types
-	public static final String DDR5 = "DDR5"; 
+
+	// Graphics Memory Types
+	public static final String DDR5 = "DDR5";
 	public static final String GDDR2 = "GDDR2";
 	public static final String GDDR3 = "GDDR3";
 	public static final String GDDR4 = "GDDR4";
 	public static final String GDDR5 = "GDDR5";
 	public static final String GDDR5X = "GDDR5X";
-	public static final String HBM = "HBM"; 
-	public static final String SDRAM = "SDRAM"; 
+	public static final String HBM = "HBM";
+	public static final String SDRAM = "SDRAM";
 }

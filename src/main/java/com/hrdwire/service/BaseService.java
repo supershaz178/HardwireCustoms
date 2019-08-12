ad6c5dc4262f39f9421b27181a574558e5a9e2a8
supershaz178 <supershaz178@users.noreply.github.com>
2019-08-12 17:43:59 -0500

    Delete extra interface methods from BaseService

--- src/main/java/com/hrdwire/service/BaseService.java
+++ src/main/java/com/hrdwire/service/BaseService.java
@@ -3,10 +3,4 @@ package com.hrdwire.service;
 public interface BaseService <T>{
 	
 	public T selectById(Integer id); 
-	
-	public void delete( T deleteObj); 
-	
-	public void create( T createObj); 
-	
-	public void update( T updateObj); 
 }
